9604a35ad96465177cf39fd75b3785a4dadc260d
HOMZIKx <ciborex12@gmail.com>
2026-09-07 11:14:33 -0700

    fix(discord): remove duplicate membership route

--- apps/discord-gateway/src/interface/app.module.ts
+++ apps/discord-gateway/src/interface/app.module.ts
@@ -23,7 +23,6 @@ import { TechnikaGuildPanelsController } from './http/technika-guild-panels.cont
 import { TechnikaGuildsController } from './http/technika-guilds.controller.js';
 import { TechnikaPanelsController } from './http/technika-panels.controller.js';
 import { MemberActivityController } from './http/member-activity.controller.js';
-import { GuildMembershipController } from './http/guild-membership.controller.js';
 import { MemberActivityStore } from '../application/member-activity/member-activity-store.js';
 import { MemberActivityCollector } from '../application/member-activity/member-activity-collector.js';
 import { resolveActiveBotConfig } from '../application/technika/active-bot-config.js';
@@ -46,7 +45,6 @@ function resolveTechnikaDataDir(config: DiscordGatewayConfig): string {
     TechnikaCapabilitiesController,
     TechnikaConfigController,
     TechnikaGuildsController,
-    GuildMembershipController,
     TechnikaGuildPanelsController,
     TechnikaPanelsController,
     MemberActivityController,
