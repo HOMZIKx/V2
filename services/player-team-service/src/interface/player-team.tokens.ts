4ff76cd4f1c2653b55fce2743858b3adb240d996
HOMZIKx <ciborex12@gmail.com>
2026-09-08 04:30:08 -0700

    feat(player-team): register team invitations token

--- services/player-team-service/src/interface/player-team.tokens.ts
+++ services/player-team-service/src/interface/player-team.tokens.ts
@@ -4,3 +4,4 @@ export const PLAYER_TEAM_ENV: InjectionToken = 'PLAYER_TEAM_ENV';
 export const PLAYER_TEAM_STATE_USE_CASES: InjectionToken = 'PLAYER_TEAM_STATE_USE_CASES';
 export const HUNT_ROOMS_USE_CASES: InjectionToken = 'HUNT_ROOMS_USE_CASES';
 export const FIXED_HUNT_ROOMS_USE_CASES: InjectionToken = 'FIXED_HUNT_ROOMS_USE_CASES';
+export const TEAM_INVITATIONS_USE_CASES: InjectionToken = 'TEAM_INVITATIONS_USE_CASES';
