b4b303a3714ef19f05fbb1ad46d2479dd729e991
HOMZIKx <ciborex12@gmail.com>
2026-09-08 08:00:23 -0700

    refactor(player-team): register metin general hunt token

--- services/player-team-service/src/interface/player-team.tokens.ts
+++ services/player-team-service/src/interface/player-team.tokens.ts
@@ -3,5 +3,5 @@ import { type InjectionToken } from '@nestjs/common';
 export const PLAYER_TEAM_ENV: InjectionToken = 'PLAYER_TEAM_ENV';
 export const PLAYER_TEAM_STATE_USE_CASES: InjectionToken = 'PLAYER_TEAM_STATE_USE_CASES';
 export const HUNT_ROOMS_USE_CASES: InjectionToken = 'HUNT_ROOMS_USE_CASES';
-export const FIXED_HUNT_ROOMS_USE_CASES: InjectionToken = 'FIXED_HUNT_ROOMS_USE_CASES';
+export const METIN_GENERAL_HUNTS_USE_CASES: InjectionToken = 'METIN_GENERAL_HUNTS_USE_CASES';
 export const TEAM_INVITATIONS_USE_CASES: InjectionToken = 'TEAM_INVITATIONS_USE_CASES';
